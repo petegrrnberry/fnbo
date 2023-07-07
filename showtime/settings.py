85fbc855ee041acaf50736df832c874f70b89965
karenffff <karenfurst6@gmail.com>
2023-07-07 20:06:58 +0100

    database_url

--- showtime/settings.py
+++ showtime/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
 }
 
 #DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
