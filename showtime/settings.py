40ce91e4f1c515cdca44957fdc74a1f089776194
karenffff <karenfurst6@gmail.com>
2023-07-07 19:46:23 +0100

    development

--- showtime/settings.py
+++ showtime/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'showtime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
- #       'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'showtime', 
+        'USER': 'postgres',
+        'PASSWORD': 'Ladenposse3',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
+}
 
 #DATABASES['default'] = dj_database_url.config()
 
