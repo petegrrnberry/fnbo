30cac3c025cc6f77b5902ee4a2e97f9e17ee57f3
karenffff <karenfurst6@gmail.com>
2023-07-07 21:30:48 +0100

    build

--- showtime/settings.py
+++ showtime/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'/ "static"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
